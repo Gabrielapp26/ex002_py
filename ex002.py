1a4c533a83134bd63a08eb3c826aa8ba4c235be5
Gabriela Pellin Perosa <gpp260203@gmail.com>
2022-12-27 15:40:28 +0000

    Exercicio 2

---
+++ ex002.py
@@ -0,0 +1,7 @@
+#Desafio 01
+#Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+print("Escreva um número e vejo seu antecessor e sucessor")
+n = int(input("Digite um número: "))
+a = n-1
+s = n+1
+print("{} antecessor: {} e sucessor: {}".format(n,a,s))
